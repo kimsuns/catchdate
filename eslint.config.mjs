70ab0328b4141548dc8595e10a75f58809bb2430
kimsuns <kimksy27@gmail.com>
2024-12-24 16:38:19 +0900

    feat : eslint setting change

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,18 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // @typescript-eslint/no-unused-vars 설정
+      "@typescript-eslint/no-unused-vars": [
+        "warn", // "error" 대신 "warn"으로 설정 (경고로 표시)
+        {
+          args: "none", // 함수 매개변수는 무시
+          vars: "all", // 모든 선언된 변수를 확인
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
