f325471784b91dc072c7e2aa37ce70ea5522743f
kimsuns <kimksy27@gmail.com>
2025-01-09 19:30:03 +0900

    .

--- app/moimSelectdate/components/SelectDate.tsx
+++ app/moimSelectdate/components/SelectDate.tsx
@@ -1,6 +1,4 @@
 import Calendar from "@/app/components/Calendar/Calendar";
-import { MoimMemberType } from "@/app/type/type";
-import { useEffect, useState } from "react";
 
 interface SelectDateProps {
   selectMember: string;
@@ -17,18 +15,7 @@ export default function SelectDate({
   time,
   onDateSelect,
 }: SelectDateProps) {
-  // const [member, setMember] = useState<MoimMemberType>(selectMember);
-  const [selectDate, setSelectDate] = useState([]);
-  console.log("선택한 멤버", selectMember);
-
   const handleDateSelect = (dates: Date[]) => {
-    // setMember((prev: MoimMemberType): MoimMemberType => {
-    //   const updateData = {
-    //     ...prev,
-    //     dates: dates,
-    //   };
-    //   return updateData;
-    // });
     onDateSelect(dates);
   };
 
@@ -40,10 +27,10 @@ export default function SelectDate({
           {startDate ? new Date(startDate).toLocaleDateString() : "시작 날짜"} ~{" "}
           {endDate ? new Date(endDate).toLocaleDateString() : "종료 날짜"}
         </span>
-        <span>' 중 하루</span>
+        <span>'일 중, 하루</span>
         <div>
           <span className="text-[#3a8bb5] font-bold">{time}</span>
-          <span>시간에 모일 예정입니다.</span>
+          <span>에 모일 예정입니다.</span>
         </div>
       </section>
 
