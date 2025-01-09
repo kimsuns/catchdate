827b88583158f80952968dd87c48efbe50b01794
kimsuns <kimksy27@gmail.com>
2025-01-09 15:57:28 +0900

    [#6] feat : add checkbox of name select

--- app/moimSelectdate/components/SelectName.tsx
+++ app/moimSelectdate/components/SelectName.tsx
@@ -20,19 +20,36 @@ export default function SelectName({
     onSelectMember(item);
   };
 
+  console.log("멤버 정보", member);
+
   return (
-    <div className="flex flex-col gap-10">
+    <div className="flex flex-col gap-10 items-center">
       <div className="text-center">
         {onSelectAll
           ? "모두 선택하였습니다. 날짜를 확인하세요."
           : "이름을 선택하세요."}
       </div>
-      <div className="grid grid-cols-2 gap-2 place-items-center">
+      <div className="grid grid-cols-2 gap-3">
         {Array.isArray(member) &&
           member.map((item) => (
             <div key={item.memberId} className="flex gap-[2px]">
               <button onClick={() => handleClickName(item)}>
-                v{item.name}
+                <div className="flex gap-1">
+                  {item.choose ? (
+                    <img src="check-completed.svg" alt="선택 완료된 이름" />
+                  ) : (
+                    <img
+                      src={
+                        item.name === selectName
+                          ? "check.svg"
+                          : "check-before.png"
+                      }
+                      alt={`${item.name}이름 선택`}
+                      className="w-[20px] h-[20px]"
+                    />
+                  )}
+                  {item.name}
+                </div>
               </button>
             </div>
           ))}
