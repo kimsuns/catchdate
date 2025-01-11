e00de1faa5355ec9a293906405d2374be37a2a16
kimsuns <kimksy27@gmail.com>
2025-01-11 13:50:52 +0900

    [#7] feat: add date in moimTheday page

--- app/moimTheday/page.tsx
+++ app/moimTheday/page.tsx
@@ -37,19 +37,33 @@ export default function MoimTheDay() {
   }, [queryId]);
 
   return (
-    <div className="flex flex-col items-center">
-      <section className="text-black font-suit text-[28px] font-semibold leading-none">
+    <div className="flex flex-col items-center h-full">
+      <header className="text-black font-suit text-[28px] font-semibold leading-none">
         모임 날짜 확인하기
-      </section>
-      <section className="flex flex-col  items-center p-6 justify-center  gap-12 self-stretch mt-6 mb-6 rounded-[2px] bg-[#F6F5F2]">
-        <div>기간 연말 모임</div>
-        <div>참여자 목록</div>
-        <div>모두 가능한 날짜</div>
-      </section>
-      <section>
+      </header>
+      <main className="flex-1 overflow-y-auto flex-col  items-center p-6 justify-center self-stretch mt-6 mb-6 rounded-[2px] bg-[#F6F5F2] scrollbar-gutter-stable no-scrollbar">
+        <div className="font-bold text-[18px] text-center uppercase mb-4">
+          {moimData.title && (
+            <span className="text-[#3a8bb5]">{moimData.title}</span>
+          )}
+        </div>
+        <div className="flex gap-2">
+          모임 참여자 :
+          {moimData.members.map((item) => (
+            <div>{item.name}</div>
+          ))}
+        </div>
+        <div>
+          모두 가능한 날짜
+          {moimData.pickDate.map((item) => (
+            <div>{new Date(item.date).toLocaleDateString()}</div>
+          ))}
+        </div>
+      </main>
+      <footer>
         <button>이미지 저장</button>
         <button>공유하기</button>
-      </section>
+      </footer>
     </div>
   );
 }
