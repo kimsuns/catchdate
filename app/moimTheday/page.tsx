d369f9a4554c097a8eb5ee66dc373fde88f5eb10
kimsuns <kimksy27@gmail.com>
2025-01-12 23:23:25 +0900

    [#7] feat: add main move router

--- app/moimTheday/page.tsx
+++ app/moimTheday/page.tsx
@@ -5,6 +5,7 @@ import { getMoimApi } from "../api/api";
 import TheDay from "./components/TheDay";
 import Button from "../components/Button/Button";
 import { MoimDataType } from "../type/type";
+import { useRouter } from "next/navigation";
 // allPickDate 날짜
 // 67821836b095e13967864d9b
 
@@ -24,6 +25,7 @@ export default function MoimTheDay() {
   });
   const [queryId, setQueryId] = useState<string | null>(null);
   const [unableMember, setUnableMember] = useState<string[]>([]);
+  const router = useRouter();
 
   useEffect(() => {
     const searchParams = new URLSearchParams(window.location.search);
@@ -90,9 +92,12 @@ export default function MoimTheDay() {
           <Button onClick={() => {}}>이미지 저장</Button>
           <Button onClick={() => {}}>공유하기</Button>
         </div>
-        <div className="text-[12px] text-center text-gray-500 underline">
+        <button
+          onClick={() => router.push("/")}
+          className="text-[12px] text-center text-gray-500 underline"
+        >
           새로운 모임 날짜 잡으러 가기
-        </div>
+        </button>
       </footer>
     </div>
   );
